c32f235c50f024ad6b1f286fb3eff49276a47ca7
Alexandre Ramos <aramos1069gmail.com>
2025-09-11 14:45:54 -0300

    atualizações 11/09 seletor de data mobile novo ajuste

--- src/components/perfil/EditPessoalModal.tsx
+++ src/components/perfil/EditPessoalModal.tsx
@@ -14,6 +14,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { DatePicker } from "@/components/ui/date-picker";
+import { PhoneInput } from "@/components/ui/phone-input";
 import { Button } from "@/components/ui/button";
 import {
   Select,
